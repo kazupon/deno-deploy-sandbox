5c74a7d362d5e83b743b0b656a7ec3e45c08cc61
kazuya kawaguchi <kawakazu80@gmail.com>
2021-04-10 01:20:16 +0900

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
+  const response = new Response("Hello World! Deno!", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
